813ea05f0fcc20cb343508ac112e45228164b79b
Priyanshs-ingh <157615576+Priyanshs-ingh@users.noreply.github.com>
2024-09-18 16:22:58 +0530

    creating app.py

--- app.py
+++ app.py
@@ -1,12 +1,35 @@
-from sklearn.ensemble import GradientBoostingRegressor
-import joblib
+from flask import Flask, request, jsonify, render_template
+from joblib import load
+import numpy as np
 
-# Example: Train a model
-model = GradientBoostingRegressor()
-model.fit(X_train, y_train)
+app = Flask(__name__)
 
-# Save the model
-joblib.dump(model, r'C:\Users\priyansh singh\Documents\GitHub\age_detection_project\model.pkl')
+# Load the model
+model = load('optimized_gbm.pkl')
 
-# Later...load the model
-loaded_model = joblib.load(r'C:\Users\priyansh singh\Documents\GitHub\age_detection_project\model.pkl')
+@app.route('/')
+def home():
+    return render_template('age_prediction.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    # Get JSON data sent from the frontend
+    data = request.get_json(force=True)
+    
+    # Assuming 'smoker_status' and 'age' are correctly sent by the frontend
+    smoker_status = data['smoker_status']
+    age = data['age']
+
+    # Convert data into the correct format for your model
+    # Assuming your model needs an array of features: [smoker_status, age]
+    # You might need to convert smoker_status to a numerical format if your model expects it
+    prediction_input = np.array([[smoker_status, age]])
+
+    # Generate predictions
+    prediction = model.predict(prediction_input)
+
+    # Send back the prediction in JSON format
+    return jsonify({'prediction': prediction[0]})
+
+if __name__ == '__main__':
+    app.run(debug=True)
